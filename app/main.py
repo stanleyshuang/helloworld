364b433e222cf2cbe9a2a7b45b68706583159517
stanleyshuang <stanleyshuang@qnap.com>
2020-12-07 09:57:55 +0800

    release QSA-20-12

--- app/main.py
+++ app/main.py
@@ -5,10 +5,25 @@
 # Project:  helloworld 1.0
 # Date:     2020-12-04
 #
+import sys
 from util.util_text_file import get_lines
 
-print('Hello World..\n')
+### get argv[1] as input
+if len(sys.argv) >=2 and sys.argv[1]:
+	input_file = sys.argv[1]
+else:
+	input_file = "./data/sample.txt"
 
-all_lines = get_lines("./data/sample.txt")
+### read file in all_lines
+all_lines = get_lines(input_file)
+
+### process all lines
 for line in all_lines:
+    line = line.replace("<li style=\"font-weight: 400;\">", "<li>")
+    line = line.replace("<span style=\"font-weight: 400;\">", "")
+    line = line.replace("</span>", "")
+    line = line.replace("<p><strong>Summary</strong></p>", "<h3>Summary</h3>")
+    line = line.replace("<p><strong>Recommendation</strong></p>", "<h3>Recommendation</h3>")
+    line = line.replace("<li>Open the <strong>App Center</strong> and then click .<br>", "<li>Open the <strong>App Center</strong> and then click <img src=\"https://www.qnap.com/i/_upload/support/images/magnifier.png\" style=\"width: 18px;\"> .<br>.<br>")
+
     print(line)
