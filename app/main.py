ecdfdfa3c8d03320c57ac10581aaf9e014dd0638
Stanley S Huang <stanleyshuang@qnap.com>
2021-05-16 15:35:47 +0800

    new format v4

--- app/main.py
+++ app/main.py
@@ -16,9 +16,9 @@ def output_text(filepath, content):
 
 ### get argv[1] as input
 if len(sys.argv) >=2 and sys.argv[1]:
-	input_file = sys.argv[1]
+    input_file = sys.argv[1]
 else:
-	input_file = "./data/sample.txt"
+    input_file = "./data/sample.txt"
 
 ### read file in all_lines
 all_lines = get_lines(input_file)
